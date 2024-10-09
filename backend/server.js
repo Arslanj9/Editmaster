11d48b09b2055098cb501b8710e32eb099076afb
Arslan <91810925+Arslanj9@users.noreply.github.com>
2024-10-09 14:52:56 +0500

    Update cors inside server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: [],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }
+));
 app.use(express.json())
 
 // MongoDB Connection
