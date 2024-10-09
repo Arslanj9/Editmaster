c4b7f67d9badddcf1524b661bf70d25fc7731370
Arslan <91810925+Arslanj9@users.noreply.github.com>
2024-10-09 15:05:33 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors(
   {
-    origin: [],
+    origin: ["https://editmaster-livid.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   }
